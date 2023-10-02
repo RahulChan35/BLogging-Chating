80e6d4faf0c804bfa8f42f1849df91e3c5931b56
RahulChan35 <rc2658535@gmail.com>
2023-10-02 13:18:44 +0530

    second commit

--- server.js
+++ server.js
@@ -15,8 +15,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST", "PATCH", "DELETE"],
+    origin: "*",
   },
 });
 
