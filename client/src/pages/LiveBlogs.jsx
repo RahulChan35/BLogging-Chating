ff91b115018cc1b902937061f50f5363aceb83e9
RahulChan35 <rc2658535@gmail.com>
2023-10-02 14:16:58 +0530

    commit

--- client/src/pages/LiveBlogs.jsx
+++ client/src/pages/LiveBlogs.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import io from "socket.io-client";
+import { io } from "socket.io-client";
 
 const socket = io("http://localhost:5100");
 
--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: "*",
+    origin: "http://localhost:5173",
   },
 });
 
