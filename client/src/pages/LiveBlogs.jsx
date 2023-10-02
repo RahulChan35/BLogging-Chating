7b98600a336ee19cdfcd1d180108e80f5a96f637
RahulChan35 <rc2658535@gmail.com>
2023-10-02 14:28:36 +0530

    commit

--- client/src/pages/LiveBlogs.jsx
+++ client/src/pages/LiveBlogs.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5100");
+const socket = io("http://localhost:5100", { autoConnect: false });
+socket.connect();
 
 import SingleMessage from "../components/SingleMessage";
 
