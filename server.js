35598cf2520fe46af6b85b525740cb7f8bd1ba77
RahulChan35 <rc2658535@gmail.com>
2023-10-02 14:43:18 +0530

    commit

--- server.js
+++ server.js
@@ -14,8 +14,10 @@ import { Server } from "socket.io";
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
+  allowEIO3: true,
   cors: {
-    origin: "http://localhost:5173",
+    origin: true,
+    credentials: true,
   },
 });
 
